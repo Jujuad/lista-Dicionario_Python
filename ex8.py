538498fc9c3eaf6e18fe72e83238a5d95bacc94f
Julia Andrade <72460406+Jujuad@users.noreply.github.com>
2023-05-08 00:03:07 -0300

    Delete ex8.py

--- ex8.py
+++
@@ -1,4 +0,0 @@
-dicionario1 = {1: 'teste1', 2: 'teste2', 3: 'teste3'}
-dicionario2 = {'teste4': 4, 'teste5': 5, 'teste6': 6}
-dicionario_misto = {**dicionario1, **dicionario2}
-print(dicionario_misto)
